7af588b33ebaaa8ff499d65324926328f1b410d9
akannika <a.kannika80@gmail.com>
2025-11-02 18:47:20 -0800

    Create the model

---
+++ model/main.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+
+# cleaning the data
+def get_claen_data():
+    data = pd.read_csv("../data/data.csv")
+    # drop unnamed and id columns
+    data = data.drop(["Unnamed: 32", 'id'], axis=1)
+
+    #Diagnosis (M = malignant, B = benign)
+    data['diagnosis'] = data['diagnosis'].map({'M': 1, 'B': 0})
+
+    return data
+
+# use the LogicRegression to create the model
+def create_model(data):
+    X = data.drop(['diagnosis'], axis=1)
+    y = data['diagnosis']
+
+    #  use standard scalar to make the data to be on the same scale
+    scalar = StandardScaler()
+    # scale the data
+    X = scalar.fit_transform(X)
+
+    # split the data into train and test datasets (80%:20%)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    # train the model
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+
+    # test the model
+    y_pred = model.predict(X_test)
+    print("Accuracy of our model: ", accuracy_score(y_test, y_pred))
+    print("Classification report: \n", classification_report(y_test, y_pred))
+
+    return model, scalar   
+
+
+# main model
+def main():
+    # clean the data
+    data = get_claen_data()
+    # print to see the data
+    # print(data.head())
+    # print(data.tail())
+    # print(data.info())
+
+    # create the model
+    model = create_model(data)
+
+    
+
+if __name__== "__main__":
+    main()
